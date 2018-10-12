c4c08574ee8fa43c303fcdab414031c08ac80ad0
nevliin <nevliin.white@gmail.com>
2018-10-13 00:04:52 +0200

    Added routes for accounts and transactions

--- server/src/routes/routes.ts
+++ server/src/routes/routes.ts
@@ -2,6 +2,8 @@ import {Application} from "express";
 import {authRouter} from "./auth-routes";
 import {CRUDConstructor} from "../core/crud-constructor";
 import {CategoryModel} from "../models/category.model";
+import {AccountModel} from "../models/account.model";
+import {TransactionModel} from "../models/transaction.model";
 
 const express = require('express');
 export const router = express.Router();
@@ -12,7 +14,13 @@ export class Routes {
 
         app.use('/auth', authRouter);
 
-        const categoryModelCRUD: CRUDConstructor<CategoryModel> = new CRUDConstructor(new CategoryModel(), 'fin_category', {softDelete: true})
+        const categoryModelCRUD: CRUDConstructor<CategoryModel> = new CRUDConstructor(new CategoryModel(), 'fin_category', {softDelete: true});
         app.use('/fin/category', categoryModelCRUD.getRouter());
+
+        const accountModelCRUD: CRUDConstructor<AccountModel> = new CRUDConstructor(new AccountModel(), 'fin_account', {softDelete: true});
+        app.use('/fin/account', accountModelCRUD.getRouter());
+
+        const transactionModelCRUD: CRUDConstructor<TransactionModel> = new CRUDConstructor(new TransactionModel(), 'fin_transaction', {softDelete: true});
+        app.use('/fin/transaction', transactionModelCRUD.getRouter());
     }
 }
