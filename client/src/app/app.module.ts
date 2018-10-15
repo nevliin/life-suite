55a229d45098954971a61546696642e78e92dfb2
nevliin <nevliin.white@gmail.com>
2018-10-15 20:23:41 +0200

    Minor changes

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -2,13 +2,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import {CoreModule} from "./core/core.module";
+import {RouterModule} from "@angular/router";
+import {AppRoutingModule} from "./app-routing.module";
+import {HttpClientModule} from "@angular/common/http";
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    CoreModule,
+    RouterModule,
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- client/src/app/core/core.module.ts
+++ client/src/app/core/core.module.ts
@@ -1,11 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {AuthModule} from "./auth/auth.module";
+import {ErrorHandlingModule} from "./error-handling/error-handling.module";
+import {FormsModule, ReactiveFormsModule} from "@angular/forms";
 
 @NgModule({
   imports: [
     CommonModule,
-    AuthModule
+    AuthModule,
+    ErrorHandlingModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   declarations: []
 })
