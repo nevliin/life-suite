1b66f1a7110b191adb39b28f20e21b76742e5a1b
nevliin <nevliin.white@gmail.com>
2018-10-19 21:33:20 +0200

    Added '/inv' to routes

--- client/src/app/app.routing.ts
+++ client/src/app/app.routing.ts
@@ -1,16 +1,26 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
 import {RouterModule, Routes} from "@angular/router";
 import {AuthLoginComponent} from "./core/auth/auth-login/auth-login.component";
 import {PageNotFoundComponent} from "./core/error-handling/page-not-found/page-not-found.component";
+import {InvListComponent} from "./inv/inv-list/inv-list.component";
 
 const routes: Routes = [
   {
     path: 'auth',
     children: [
       {
-        path: 'login',
-        component: AuthLoginComponent
+        path: 'login', component: AuthLoginComponent
+      }
+    ]
+  },
+  {
+    path: 'inv',
+    children: [
+      {
+        path: 'list', component: InvListComponent
+      },
+      {
+        path: '', redirectTo: '/list', pathMatch: 'full'
       }
     ]
   },
@@ -29,4 +39,4 @@ const routes: Routes = [
   imports: [ RouterModule.forRoot(routes) ],
   exports: [ RouterModule ]
 })
-export class AppRoutingModule { }
+export class AppRouting { }
