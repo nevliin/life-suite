5c0502f0bbcdc8fb00c75babc5d80605bd0caf01
nevliin <nevliin.white@gmail.com>
2018-10-20 18:12:47 +0200

    Added route resolvers and multiple '/inv' routes

--- client/src/app/app.routing.ts
+++ client/src/app/app.routing.ts
@@ -1,8 +1,12 @@
-import { NgModule } from '@angular/core';
-import {RouterModule, Routes} from "@angular/router";
+import {Injectable, NgModule} from '@angular/core';
+import {ActivatedRouteSnapshot, Resolve, RouterModule, RouterStateSnapshot, Routes} from "@angular/router";
 import {AuthLoginComponent} from "./core/auth/auth-login/auth-login.component";
 import {PageNotFoundComponent} from "./core/error-handling/page-not-found/page-not-found.component";
 import {InvListComponent} from "./inv/inv-list/inv-list.component";
+import {InvComparisonComponent} from "./inv/inv-comparison/inv-comparison.component";
+import {CurrentMenuResolver} from "./core/menu/current-menu.resolver";
+import {InvTargetComponent} from "./inv/inv-target/inv-target.component";
+import {InvExpirationsComponent} from "./inv/inv-expirations/inv-expirations.component";
 
 const routes: Routes = [
   {
@@ -15,12 +19,24 @@ const routes: Routes = [
   },
   {
     path: 'inv',
+    resolve: {
+      menu: CurrentMenuResolver
+    },
     children: [
       {
         path: 'list', component: InvListComponent
       },
       {
-        path: '', redirectTo: '/list', pathMatch: 'full'
+        path: 'comparison', component: InvComparisonComponent
+      },
+      {
+        path: 'target', component: InvTargetComponent
+      },
+      {
+        path: 'expirations', component: InvExpirationsComponent
+      },
+      {
+        path: '', redirectTo: 'list', pathMatch: 'full'
       }
     ]
   },
