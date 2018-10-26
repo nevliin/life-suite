f8cbd978e1d9e79af6dcccf9c8adaaaeb9dfeb44
nevliin <nevliin.white@gmail.com>
2018-10-27 01:09:28 +0200

    Added ValidatorUtil for REST input

---
+++ server/src/utils/validator/validator.util.ts
@@ -0,0 +1,16 @@
+import {ErrorCodeUtil} from "../error-code/error-code.util";
+
+export class ValidatorUtil {
+
+    static valNum(num: number) {
+        if(Number.isNaN(Number.parseFloat(num.toString()))) {
+            ErrorCodeUtil.findErrorCodeAndThrow('INVALID_PARAMETER');
+        }
+    }
+
+    static valDate(date: Date) {
+        if(!(date instanceof Date)) {
+            ErrorCodeUtil.findErrorCodeAndThrow('INVALID_PARAMETER');
+        }
+    }
+}
