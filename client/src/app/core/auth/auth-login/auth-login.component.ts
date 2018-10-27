97fe3ecd0e569af3a19940f415e4b4e19d1a6d00
nevliin <nevliin.white@gmail.com>
2018-10-27 21:52:19 +0200

    Separated AuthLoginWrapper and AuthLogin, added styling to AuthLogin

--- client/src/app/core/auth/auth-login/auth-login.component.ts
+++ client/src/app/core/auth/auth-login/auth-login.component.ts
@@ -2,39 +2,37 @@ import {Component, OnInit} from '@angular/core';
 import {FormBuilder, FormGroup, Validators} from "@angular/forms";
 import {HttpClient} from "@angular/common/http";
 import {Router} from "@angular/router";
+import {AuthService} from "../auth.service";
 
 @Component({
-  selector: 'app-auth-login',
-  templateUrl: './auth-login.component.html',
-  styleUrls: ['./auth-login.component.css']
+    selector: 'app-auth-login',
+    templateUrl: './auth-login.component.html',
+    styleUrls: ['./auth-login.component.css']
 })
 export class AuthLoginComponent implements OnInit {
-  loginForm: FormGroup = this.fb.group({
-    'username': ['', Validators.required],
-    'password': ['', Validators.required]
-  });
+    loginForm: FormGroup = this.fb.group({
+        'username': ['', Validators.required],
+        'password': ['', Validators.required]
+    });
 
-  constructor(
-    readonly fb: FormBuilder,
-    readonly http: HttpClient,
-    readonly router: Router
-  ) {
+    constructor(
+        readonly fb: FormBuilder,
+        readonly http: HttpClient,
+        readonly router: Router,
+        readonly authService: AuthService
+    ) {
 
-  }
+    }
 
-  ngOnInit() {
-  }
+    ngOnInit() {
+    }
 
-  async submit() {
-    console.log(this.loginForm.get('username')!.value);
-    this.http.post('/api/auth/login', {
-      username: this.loginForm.get('username').value,
-      password: this.loginForm.get('password').value
-    }).subscribe(response => console.log(response));
-  }
+    async submit() {
+        await this.authService.logIn(this.loginForm.get('username')!.value, this.loginForm.get('password')!.value);
+    }
 
-  async cancel() {
-    await this.router.navigate([]);
-  }
+    async cancel() {
+        await this.router.navigate([]);
+    }
 
 }
