35c5435773e6b3f99dbf25d8fd27da0b7556bb5e
nevliin <nevliin.white@gmail.com>
2018-10-27 21:53:54 +0200

    Added new component

--- client/src/app/core/auth/auth.module.ts
+++ client/src/app/core/auth/auth.module.ts
@@ -3,15 +3,19 @@ import { CommonModule } from '@angular/common';
 import { AuthLoginComponent } from './auth-login/auth-login.component';
 import {ReactiveFormsModule} from "@angular/forms";
 import {RouterModule} from "@angular/router";
+import { AuthLoginWrapperComponent } from './auth-login-wrapper/auth-login-wrapper.component';
+import {ButtonModule} from "primeng/button";
 
 @NgModule({
   imports: [
     CommonModule,
     ReactiveFormsModule,
-    RouterModule
+    RouterModule,
+      ButtonModule
   ],
   declarations: [
     AuthLoginComponent,
+    AuthLoginWrapperComponent,
   ]
 })
 export class AuthModule { }
