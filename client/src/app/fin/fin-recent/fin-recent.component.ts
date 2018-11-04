c00a1462a220a998a6544138b68de1996a4c06d5
nevliin <nevliin.white@gmail.com>
2018-11-04 13:10:42 +0100

    Styled transactions with grid

--- client/src/app/fin/fin-recent/fin-recent.component.ts
+++ client/src/app/fin/fin-recent/fin-recent.component.ts
@@ -9,7 +9,7 @@ import {FinTransaction} from "../fin-transaction";
 })
 export class FinRecentComponent implements OnInit {
 
-    recentTransactions: FinTransaction[] = [];
+    recentTransactions: FinTransactionDisplay[] = [];
 
     constructor(
         readonly finService: FinService
@@ -17,8 +17,32 @@ export class FinRecentComponent implements OnInit {
     }
 
     async ngOnInit() {
-        this.recentTransactions = await this.finService.getRecentTransactions();
+        const tempArray: FinTransaction[] = await this.finService.getRecentTransactions();
+        for(const transaction of tempArray) {
+            this.recentTransactions.push(new FinTransactionDisplay(
+                transaction,
+                (await this.finService.getAccountById(transaction.account)).name,
+                (await this.finService.getAccountById(transaction.contra_account)).name)
+            );
+        }
     }
 
+}
 
+class FinTransactionDisplay extends FinTransaction {
+    accountName: string;
+    contraAccountName: string;
+
+    constructor(transaction: FinTransaction, accountName: string, contraAccountName: string) {
+        super();
+        this.note = transaction.note;
+        this.created_on = transaction.created_on;
+        this.amount = transaction.amount;
+        this.contra_account = transaction.contra_account;
+        this.account = transaction.account;
+        this.executed_on = transaction.executed_on;
+        this.id = transaction.id;
+        this.accountName = accountName;
+        this.contraAccountName = contraAccountName;
+    }
 }
