e00d6a03bceb033e31c6119c16120b70a13f317b
nevliin <nevliin.white@gmail.com>
2018-12-02 12:27:49 +0100

    minor

--- server/sql/fin/setup-pg.sql
+++ server/sql/fin/setup-pg.sql
@@ -7,57 +7,57 @@ DROP TABLE IF EXISTS fin_interval_type;
 
 CREATE TABLE fin_interval_type(
   id SERIAL PRIMARY KEY,
-  name VARCHAR(255)
+  name TEXT
 );
 
 CREATE TABLE fin_category (
   id SERIAL PRIMARY KEY,
-  name VARCHAR(63) NOT NULL,
-  active INT(1) NOT NULL,
-  valid INT(1) NOT NULL DEFAULT 1
+  name TEXT NOT NULL,
+  active INT NOT NULL,
+  valid INT NOT NULL DEFAULT 1
 );
 
 CREATE TABLE fin_account(
-  id INT(4) PRIMARY KEY ON UPDATE CASCADE,
-  name VARCHAR(127),
-  note VARCHAR(255),
-  parent_account INT(4) REFERENCES fin_account(id) ON UPDATE CASCADE,
+  id INT PRIMARY KEY ON UPDATE CASCADE,
+  name TEXT,
+  note TEXT,
+  parent_account INT REFERENCES fin_account(id) ON UPDATE CASCADE,
   category_id INT REFERENCES fin_category(id),
-  valid INT(1) NOT NULL DEFAULT 1,
+  valid INT NOT NULL DEFAULT 1,
   created_on TIMESTAMP DEFAULT NOW()
 );
 
 CREATE TABLE fin_planned_transaction(
   id SERIAL PRIMARY KEY ,
-  account INT(4) REFERENCES fin_account(id) ON UPDATE CASCADE,
-  contra_account INT(4) REFERENCES fin_account(id) ON UPDATE CASCADE,
+  account INT REFERENCES fin_account(id) ON UPDATE CASCADE,
+  contra_account INT REFERENCES fin_account(id) ON UPDATE CASCADE,
   amount DECIMAL(30, 2) NOT NULL,
-  note VARCHAR(255),
+  note TEXT,
   start TIMESTAMP NOT NULL,
   finish TIMESTAMP NOT NULL,
   interval_length INT NOT NULL,
   interval_type_id INT REFERENCES fin_interval_type(id),
-  deactivated INT(1) NOT NULL DEFAULT 0,
+  deactivated INT NOT NULL DEFAULT 0,
   planned_on TIMESTAMP DEFAULT NOW()
 );
 
 CREATE TABLE fin_transaction(
   id SERIAL PRIMARY KEY,
-  account INT(4) REFERENCES fin_account(id) ON UPDATE CASCADE,
-  contra_account INT(4) REFERENCES fin_account(id) ON UPDATE CASCADE,
+  account INT REFERENCES fin_account(id) ON UPDATE CASCADE,
+  contra_account INT REFERENCES fin_account(id) ON UPDATE CASCADE,
   amount DECIMAL(30, 2) NOT NULL,
-  note VARCHAR(255),
+  note TEXT,
   planned_transaction_id INT REFERENCES fin_planned_transaction(id),
   created_on TIMESTAMP DEFAULT NOW(),
-  valid INT(1) NOT NULL DEFAULT 1,
+  valid INT NOT NULL DEFAULT 1,
   executed_on TIMESTAMP DEFAULT NOW()
 );
 
 CREATE TABLE fin_constraint(
   id SERIAL PRIMARY KEY,
-  name VARCHAR(255) NOT NULL,
-  definition VARCHAR(21000) CHARACTER SET utf8,
-  message VARCHAR(255),
+  name TEXT NOT NULL,
+  definition TEXT,
+  message TEXT,
   created_on TIMESTAMP DEFAULT NOW(),
-  valid INT(1) NOT NULL DEFAULT 1
+  valid INT NOT NULL DEFAULT 1
 );
