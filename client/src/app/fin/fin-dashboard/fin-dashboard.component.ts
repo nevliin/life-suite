5c6cdb22e0bb0a79cf1231d3132cef33231e4896
nevliin <nevliin.white@gmail.com>
2018-12-19 23:22:21 +0100

    feat(fin-dashboard): added expenses and income as doughnut charts

--- client/src/app/fin/fin-dashboard/fin-dashboard.component.ts
+++ client/src/app/fin/fin-dashboard/fin-dashboard.component.ts
@@ -1,15 +1,65 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {AccountBalance, FinService} from "../fin.service";
+import {chartColors} from "../../core/chart-colors";
 
 @Component({
-  selector: 'app-fin-dashboard',
-  templateUrl: './fin-dashboard.component.html',
-  styleUrls: ['./fin-dashboard.component.css']
+    selector: 'app-fin-dashboard',
+    templateUrl: './fin-dashboard.component.html',
+    styleUrls: ['./fin-dashboard.component.css']
 })
 export class FinDashboardComponent implements OnInit {
 
-  constructor() { }
+    expensesData: any;
+    incomeData: any;
+    options = {
+        responsive: false,
+        maintainAspectRatio: false
+    };
 
-  ngOnInit() {
-  }
+    constructor(
+        private readonly finService: FinService
+    ) {
+    }
+
+    async ngOnInit() {
+        let expenseBalances: AccountBalance[] = this.summarizeOther(await this.finService.getAccountBalancesByCategory(3));
+        this.expensesData = {
+            labels: expenseBalances.map(balance => balance.name),
+            datasets: [
+                {
+                    data: expenseBalances.map(balance => balance.balance),
+                    backgroundColor: chartColors
+                }]
+        };
+        let incomeBalances: AccountBalance[] = this.summarizeOther(await this.finService.getAccountBalancesByCategory(6));
+        this.incomeData = {
+            labels: incomeBalances.map(balance => balance.name),
+            datasets: [
+                {
+                    data: incomeBalances.map(balance => balance.balance),
+                    backgroundColor: chartColors
+                }]
+        };
+    }
+
+    summarizeOther(balances: AccountBalance[]): AccountBalance[] {
+        balances = balances
+            .filter(balance => balance.balance > 0)
+            .sort((a, b) => b.balance - a.balance);
+        const other = balances.slice(10);
+        balances = balances.slice(0, 10);
+
+        let sumOther = 0;
+        other.forEach(balance => {
+            sumOther += balance.balance;
+        });
+
+        balances.push({
+            balance: sumOther,
+            name: "Other",
+            id: null
+        });
+        return balances;
+    }
 
 }
