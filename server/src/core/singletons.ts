203b278d73e27440dd196e6cf82a2e6080b0a12d
nevliin <nevliin.white@gmail.com>
2019-01-28 23:31:37 +0100

    feat(user): GET /user/self for details of own user

--- server/src/core/singletons.ts
+++ server/src/core/singletons.ts
@@ -1,13 +1,15 @@
-import {ErrorCodeUtil} from "../utils/error-code/error-code.util";
-import {LoggingUtil} from "./logging/logging.util";
-import {AuthService} from "./auth/auth.service";
+import {ErrorCodeUtil} from '../utils/error-code/error-code.util';
+import {LoggingUtil} from './logging/logging.util';
+import {AuthService} from './auth/auth.service';
+import {UserService} from './user/user.service';
 
 export class Singletons {
 
     public static async init() {
         ErrorCodeUtil.init();
         LoggingUtil.init();
-        await AuthService.init();
+        AuthService.init().then().catch();
+        UserService.init().then().catch();
 
     }
 
--- server/src/core/user/user.routing.ts
+++ server/src/core/user/user.routing.ts
@@ -6,6 +6,7 @@ import {ValidatorUtil} from '../../utils/validator/validator.util';
 import {CRUDConstructor} from '../crud/crud-constructor';
 import {UserModel} from './model/user.model';
 import {IServerConfig} from '../../assets/config/server-config.model';
+import {AuthService} from '../auth/auth.service';
 
 const express = require('express');
 const config: IServerConfig = require('../../assets/config/server-config.json');
@@ -15,8 +16,14 @@ export const init = (): Router => {
 
     userRouter.get('/self', async (req: Request, res: Response, next: NextFunction) => {
         try {
-            ValidatorUtil.valNum(req.params.userId);
-            const userDetails: IUserDetailsModel = await UserService.getUserDetails(req.params.userId);
+            let userId: number | undefined;
+            if (req.cookies && req.cookies.auth_token) {
+                userId = await AuthService.verifyToken(req.cookies.auth_token);
+            }
+            let userDetails: IUserDetailsModel | undefined;
+            if (userId) {
+                userDetails = await UserService.getUserDetails(userId);
+            }
             res.status(200).send({
                 userDetails: userDetails
             });
