6507b6bebc2e482082d730416deace609cec26df
nevliin <nevliin.white@gmail.com>
2019-02-16 01:14:12 +0100

    fix(crud-constructor): GET /list now accepts options encoded as base64

--- server/src/core/crud/crud-constructor.ts
+++ server/src/core/crud/crud-constructor.ts
@@ -416,7 +416,7 @@ export class CRUDConstructor<T extends ICRUDModel, > {
         });
         router.get('/list', async (req: Request, res: Response, next: NextFunction) => {
             try {
-                const data: T[] = await this.list(req.params.options ? JSON.parse(req.params.options) : {});
+                const data: T[] = await this.list(req.query.options ? JSON.parse(Buffer.from(req.query.options, 'base64').toString()) : {});
                 res.status(200).send({
                     data: data
                 });
